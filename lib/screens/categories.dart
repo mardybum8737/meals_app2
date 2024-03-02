969176f9fe975b6d3a7ebbea70337364c34c812c
mardybum8737 <36396926+mardybum8737@users.noreply.github.com>
2024-03-02 11:49:07 +0900

    udemyFlutter_197

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,7 +6,7 @@ import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/models/category.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     required this.availableMeals,
@@ -14,8 +14,35 @@ class CategoriesScreen extends StatelessWidget {
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  // with 키워드는
+  late AnimationController _animController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animController
+        .dispose(); // to make sure that the animation controller is disposed
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -52,3 +79,83 @@ class CategoriesScreen extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// import 'package:meals/data/dummy_data.dart';
+// import 'package:meals/models/meal.dart';
+// import 'package:meals/widgets/category_grid_item.dart';
+// import 'package:meals/screens/meals.dart';
+// import 'package:meals/models/category.dart';
+
+// class CategoriesScreen extends StatefulWidget {
+//   const CategoriesScreen({
+//     super.key,
+//     required this.availableMeals,
+//   });
+
+//   final List<Meal> availableMeals;
+
+//   @override
+//   State<CategoriesScreen> createState() => _CategoriesScreenState();
+// }
+
+// class _CategoriesScreenState extends State<CategoriesScreen>
+//     with SingleTickerProviderStateMixin {
+//   late AnimationController _animController;
+
+//   @override
+//   void initState() {
+//     _animController = AnimationController(
+//       vsync: this,
+//       duration: const Duration(milliseconds: 300),
+//       lowerBound: 0,
+//       upperBound: 1,
+//     );
+//     super.initState();
+//   }
+
+//   @override
+//   void dispose() {
+//     _animController.dispose();
+//     super.dispose();
+//   }
+
+//   void _selectCategory(BuildContext context, Category category) {
+//     final filteredMeals = widget.availableMeals
+//         .where((meal) => meal.categories.contains(category.id))
+//         .toList();
+
+//     Navigator.of(context).push(
+//       MaterialPageRoute(
+//         builder: (ctx) => MealsScreen(
+//           title: category.title,
+//           meals: filteredMeals,
+//         ),
+//       ),
+//     ); // Navigator.push(context, route)
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GridView(
+//       padding: const EdgeInsets.all(24),
+//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//         crossAxisCount: 2,
+//         childAspectRatio: 3 / 2,
+//         crossAxisSpacing: 20,
+//         mainAxisSpacing: 20,
+//       ),
+//       children: [
+//         // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+//         for (final category in availableCategories)
+//           CategoryGridItem(
+//             category: category,
+//             onSelectCategory: () {
+//               _selectCategory(context, category);
+//             },
+//           )
+//       ],
+//     );
+//   }
+// }
